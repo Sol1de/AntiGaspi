3d9bc8f092010287210dcd0754dc855504f2cf48
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2024-03-21 21:29:29 +0100

    :fire: fixed faq section

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-//ouvre les balises de FAQ
-function openFaq(id) {
-    const answer = document.querySelector(`#${id}`);
+//ouvre/ferme les balises de FAQ
+function openFaq(answoriId, plusButtonId) {
+    const answer = document.querySelector(`#${answoriId}`);
     const container = answer.parentElement;
     const maxHeight = answer.scrollHeight; // permet de récupérer la hauteur de la réponse dans son état initial
+    const plusButton = document.querySelector(`#${plusButtonId}`);
 
     if (answer.style.height === '0px' || answer.style.height === '') {
         answer.style.height = `${maxHeight}px`;
+        plusButton.style.transform = 'rotate(45deg)';
     } else {
         answer.style.height = '0';
+        plusButton.style.transform = 'rotate(0deg)';
     }
 }
